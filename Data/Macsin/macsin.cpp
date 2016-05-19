a2619efeb592e86a93109f674ca675da4aafc4ff
Sergey Goremykin <GSA05@mail.ru>
2016-05-19 06:45:40 +0300

    #15: details of class defenition

--- Data/Macsin/macsin.cpp
+++ Data/Macsin/macsin.cpp
@@ -1,5 +1,105 @@
 #include "macsin.h"
 
+quint8 Macsin::getNGEO() const
+{
+    return NGEO;
+}
+
+void Macsin::setNGEO(const quint8 &value)
+{
+    NGEO = value;
+}
+
+quint8 Macsin::getNZON() const
+{
+    return NZON;
+}
+
+void Macsin::setNZON(const quint8 &value)
+{
+    NZON = value;
+}
+
+quint8 Macsin::getNGR() const
+{
+    return NGR;
+}
+
+void Macsin::setNGR(const quint8 &value)
+{
+    NGR = value;
+}
+
+quint8 Macsin::getNGRT() const
+{
+    return NGRT;
+}
+
+void Macsin::setNGRT(const quint8 &value)
+{
+    NGRT = value;
+}
+
+quint8 Macsin::getNMAT() const
+{
+    return NMAT;
+}
+
+void Macsin::setNMAT(const quint8 &value)
+{
+    NMAT = value;
+}
+
+quint16 Macsin::getNPRED() const
+{
+    return NPRED;
+}
+
+void Macsin::setNPRED(const quint16 &value)
+{
+    NPRED = value;
+}
+
+qreal Macsin::getAlbedo() const
+{
+    return albedo;
+}
+
+void Macsin::setAlbedo(const qreal &value)
+{
+    albedo = value;
+}
+
+QVector<qreal> Macsin::getRadiuses() const
+{
+    return radiuses;
+}
+
+void Macsin::setRadiuses(const QVector<qreal> &value)
+{
+    radiuses = value;
+}
+
+QVector<quint16> Macsin::getZones() const
+{
+    return zones;
+}
+
+void Macsin::setZones(const QVector<quint16> &value)
+{
+    zones = value;
+}
+
+QVector<quint8> Macsin::getOnes() const
+{
+    return ones;
+}
+
+void Macsin::setOnes(const QVector<quint8> &value)
+{
+    ones = value;
+}
+
 Macsin::Macsin()
 {
 
--- Data/Macsin/macsin.h
+++ Data/Macsin/macsin.h
@@ -5,8 +5,47 @@
 
 class Macsin : public Data
 {
+//RECORD 1
+    quint8 NGEO;
+    quint8 NZON;
+    quint8 NGR;
+    quint8 NGRT;
+    quint8 NMAT;
+    quint16 NPRED;
+
+//RECORD 2
+    qreal albedo = 1;
+
+//RECORD 3
+    QVector<qreal> radiuses;
+
+//RECORD 4
+    QVector<quint16> zones;
+
+//RECORD 5
+    QVector<quint8> ones;
 public:
     Macsin();
+    quint8 getNGEO() const;
+    void setNGEO(const quint8 &value);
+    quint8 getNZON() const;
+    void setNZON(const quint8 &value);
+    quint8 getNGR() const;
+    void setNGR(const quint8 &value);
+    quint8 getNGRT() const;
+    void setNGRT(const quint8 &value);
+    quint8 getNMAT() const;
+    void setNMAT(const quint8 &value);
+    quint16 getNPRED() const;
+    void setNPRED(const quint16 &value);
+    qreal getAlbedo() const;
+    void setAlbedo(const qreal &value);
+    QVector<qreal> getRadiuses() const;
+    void setRadiuses(const QVector<qreal> &value);
+    QVector<quint16> getZones() const;
+    void setZones(const QVector<quint16> &value);
+    QVector<quint8> getOnes() const;
+    void setOnes(const QVector<quint8> &value);
 };
 
 #endif // MACSIN_H
